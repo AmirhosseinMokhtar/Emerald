47cc37adf7b1cdce9bcabaa17a1e9eb4c1b1d098
Amirhossein Rahim <rahim.amirh@gmail.com>
2025-10-07 22:15:41 +0330

    Main Menu UI Created
    Added:
    UI Captions
    Art Prototype
    Some UI Animations
    Script for start game
    + A beautiful splash screen!

---
+++ Emerald/Assets/Scripts/StartGame.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+    public string gameSceneName;
+
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            Debug.Log("Space Pressed");
+        }
+                //SceneManager.LoadScene(gameSceneName);
+    }
+}
